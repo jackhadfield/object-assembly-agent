f1792d2baff011cf2f41232d9b6ccf1f6332d545
jackhadfield <jack103@windowslive.com>
2017-01-12 14:19:28 +0200

    Added GUI code

---
+++ object_assembly_ros/include/object_assembly_ros/guess_mode_ui.hpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <tf/transform_broadcaster.h>
+#include <base_ui.hpp>
+
+class GuessModeUI: public BaseUI
+{
+public:
+    GuessModeUI(std::vector<std::string> filenames);
+    void update_probabilities();
+    void logic();
+    void draw();
+    void onDisplay();
+    void onIdle();
+};
+
+GuessModeUI *ui_global;
